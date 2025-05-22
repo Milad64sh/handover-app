5ba767abeb04c4a274701c5c638ab93cbcb49641
Milad <m.shalikarian@gmail.com>
2025-05-22 19:29:28 +0100

    fix cors error

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   'https://www.jigsawcreativecareportal.com',
   'https://handovers-9350ee388e53.herokuapp.com/',
   'https://jigsawcreativecareportal.com',
+  'https://handovers-5d5e8.web.app'
 ];
 
 module.exports = allowedOrigins;
--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,15 +1,14 @@
 // const { trusted } = require('mongoose');
-// const allowedOrigins = require('./allowedOrigins');
+const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-  // origin: (origin, cb) => {
-  //   if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-  //     cb(null, true);
-  //   } else {
-  //     cb(new Error('Not allowed by CORS'));
-  //   }
-  // },
-  origin: '*',
+  origin: (origin, cb) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   optionsSuccessStatus: 200,
 };
